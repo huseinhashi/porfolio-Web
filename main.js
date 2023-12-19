0a81655021787921bb3ca0d7d1b67d657321e0f1
huseinhashi <huseinarahman6@gmail.com>
2023-12-19 22:45:13 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+for (tablinks of tablinks)
+    tablinks.classList.remove("active-link");
+
+for (tabcontents of tabcontents)
+    tabcontents.classList.remove("active-tab");
+
+event.currentTarget.classList.add("active-link");
+document.getElementById(tabname).classList.add("active-tab");
+}
+
+// Side Menu Script
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
